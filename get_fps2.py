345b47a805b29b2d40255d3e2b706259b038aa4b
md123-p <202412230554@nuist.edu.cn>
2026-06-02 19:42:00 +0800

    Add files via upload

---
+++ get_fps2.py
@@ -0,0 +1,68 @@
+from ultralytics import YOLO
+import warnings
+import argparse
+warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings('ignore', category=RuntimeWarning, message='divide by zero encountered in scalar divide')
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--weights',type=str, default='runs/TIB/yolo26-DPC3k2-ConverseSample-C2PSCAA/weights/best.pt', help='loading weights')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args=main()
+    model = YOLO(args.weights)
+    print("testing TensorRT")
+    results=model.val(
+              data="/media/ubuntu/DATA/md/YOLOv26n/datasets/TIB-Net/tibnet.yaml",
+              batch=1, 
+              imgsz=640,
+              device=0,
+              half=True,
+
+              project="/media/ubuntu/DATA/md/YOLOv26n/runs/detect/TIB",
+              name='yolo26-6-DPC3k2-ConverseSample-C2PSCAA')
+    
+    speed = results.speed
+    preprocess = speed['preprocess']
+    inference = speed['inference']
+    postprocess = speed['postprocess']
+    total_ms = preprocess + inference + postprocess
+    fps = 1000.0 / total_ms
+
+    print("\n" + "="*40)
+    print(f"?? final result (TensorRT Engine)")
+    print("="*40)
+    print(f"Preprocessing time: {preprocess:.2f} ms")
+    print(f"Model inference time: {inference:.2f} ms")
+    print(f"Post-processing time: {postprocess:.2f} ms")
+    print("-" * 40)
+    print(f"Single frame delay: {total_ms:.2f} ms")
+    print(f"real FPS:   {fps:.2f}")
+    print("="*40 + "\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ train1.py
@@ -0,0 +1,48 @@
+from ultralytics import RTDETR
+from ultralytics import YOLO
+import warnings
+warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings('ignore', category=RuntimeWarning, message='divide by zero encountered in scalar divide')
+
+
+if __name__ == '__main__':
+    model = YOLO("ultralytics/cfg/models/26/yolo26-DPC3k2-ConverseSample-C2PSCAA.yaml")
+    #try:
+        #model.load("yolo26n.pt")
+        #print(" yolo26n.pt ")
+    #except Exception as e:
+        #print(f" {e}")
+
+    
+    #model = YOLO("yolo26l.pt") 
+    #print("Successfully loaded original yolo26l.pt!")
+    model.train(
+        data="/media/ubuntu/DATA/njx/yolov10-main/ultralytics/cfg/datasets/VisDrone.yaml",
+        
+        epochs=300,      
+        patience=100,
+        batch=32,               
+        imgsz=640,
+        optimizer='AdamW',     
+        lr0=0.001,             
+        weight_decay=0.0005,   
+   
+        mosaic=0.5,
+        close_mosaic=10, 
+        mixup=0.1,           
+        scale=0.5,              
+        translate=0.1,          
+        device=0,                
+        workers=8,
+        project="/media/ubuntu/DATA/md/YOLOv26n/runs/visdrone",
+        name='yolo26n-DPC3k2-ConverseSample-C2PSCAA',
+        plots=True,
+        
+        pretrained=True           
+    )
+    #model.train(data="/media/ubuntu/DATA/md/yolov12-main/datasets/DUT Anti-UAV/antiuav.yaml",
+                #epochs=300,
+                #batch=16,
+                #device=1,
+                #pretrained=False)
